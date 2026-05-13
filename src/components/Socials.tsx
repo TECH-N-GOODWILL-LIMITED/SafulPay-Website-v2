8e87e3c8de49060a7e82df91e48177f6b0cf355e
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-05-13 12:28:29 +0000

    fix(images): apply inline styles to social icons for aspect ratio stability

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -21,12 +21,12 @@ function Socials({ className }: SocialsProp) {
             aria-label={`Visit SafulPay's ${social.name} page`}
             className="w-full p-2.5 bg-primary-shade-10 rounded-full transition-all hover:bg-primary-color max-md:bg-secondary-shade-10"
           >
-            <Image unoptimized
+            <Image
+              unoptimized
               src={social.icon}
               alt={`${social.name} icon`}
               width={20}
-              height={20}
-              className="w-5 h-5"
+              className="w-5 h-auto"
               aria-hidden="true"
             />
           </a>
