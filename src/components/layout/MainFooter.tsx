8019434d330097578b831539598e480571539571
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-05-18 15:13:59 +0000

    style(footer): use bg-background token instead of bg-white

--- src/components/layout/MainFooter.tsx
+++ src/components/layout/MainFooter.tsx
@@ -5,7 +5,7 @@ import bgIcon from "@/assets/images/illustrations/bg-logo-illustration.svg";
 
 function MainFooter() {
   return (
-    <div className="bg-white relative flex flex-col items-center overflow-x-clip">
+    <div className="bg-background relative flex flex-col items-center overflow-x-clip">
       <ContactUs />
       <Image
         className="max-w-147.5 absolute right-[-14%] top-[-9%] max-md:max-w-120 max-sm:max-w-100 opacity-80 -z-10"
